fc15c19ba47d8bc2e21b18a1f703f5eecf15d6a8
lucasboot <varela.alves@outlook.com>
2022-05-28 18:50:28 -0300

    java m2 to go

---
+++ Trilha Java/Angular - M2/php/cadastrar.php
@@ -0,0 +1,31 @@
+<?php
+//Incluir conexão
+include("conexao.php");
+
+//Obter dados
+
+$obterDados = file_get_contents("php://input");
+
+//Extrair os dados do JSON
+$extrair = json_decode($obterDados);
+
+//Separar os dados
+$nomeCurso = $extrair->cursos->nomeCurso;
+$valorCurso = $extrair->cursos->valorCurso;
+
+
+//SQL
+$sql = "INSERT INTO cursos (nomeCurso, valorCurso) VALUES ('$nomeCurso', $valorCurso)";
+mysqli_query($conexao, $sql);
+
+
+//Exportar os dados cadastrados
+$curso = [
+    'nomeCurso' => $nomeCurso,
+    'valorCurso' => $valorCurso
+]
+
+json_encode(['curso']=>$curso);
+
+
+?>
